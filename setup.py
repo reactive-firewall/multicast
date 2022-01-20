fd8772ec0291515be48a52b40216d92b1f6c0f31
reactive-firewall <reactive-firewall@users.noreply.github.com>
2022-01-19 21:13:18 -0800

    [BREAKS] EOL python 2.x

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # ..........................................
-# http://www.github.com/reactive-firewall/multicast/LICENSE.md
+# http://www.github.com/reactive-firewall/multicast/LICENSE
 # ..........................................
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
