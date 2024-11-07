cf7d73729dac108f51eedd1282eb0902412ea1b4
Mr. Walls <reactive-firewall@users.noreply.github.com>
2024-11-07 14:13:32 -0800

    [UPDATE] Version Bump
    Changes in file multicast/__init__.py:
     - minor Version Bump
    
    Changes in file setup.cfg:
     - minor Version Bump
    
    Changes in file setup.py:
     - minor Version Bump

--- setup.py
+++ setup.py
@@ -23,11 +23,16 @@ Minimal Acceptance Testing:
 
 	Testcase 0: Just set up test fixtures by importing multicast.
 
+		>>> __package__ is not None
+		True
+		>>>
+
 		>>> import multicast
 		>>>
 		>>> multicast.__package__ is not None
 		True
 		>>>
+
 
 """
 
@@ -46,6 +51,15 @@ try:
 			from setuptools.config.setupcfg import read_configuration
 except Exception as err:
 	raise NotImplementedError("""[CWE-440] Not Implemented.""") from err
+
+
+__package__ = """setup"""  # skipcq: PYL-W0622
+
+
+__module__ = """setup"""  # skipcq: PYL-W0622
+
+
+__name__ = """setup"""  # skipcq: PYL-W0622
 
 
 def readFile(filename):
