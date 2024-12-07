75c1d96abe31970914dfb0a5e5209997193c3839
Mr. Walls <reactive-firewall@users.noreply.github.com>
2024-12-06 22:33:24 -0800

    [UPDATE] Version bump to v2.0.3
    Changes in file Makefile:
     - MANIFEST.in now excludes dangerous package.json file used only in testing
    
    Changes in file docs/conf.py:
     - version bump to 2.0.3-RC-1
    
    Changes in file multicast/__init__.py:
     - version bump to 2.0.3-RC-1
    
    Changes in file setup.cfg:
     - version bump to 2.0.3-RC-1

--- multicast/__init__.py
+++ multicast/__init__.py
@@ -93,7 +93,7 @@ __name__ = """multicast"""  # skipcq: PYL-W0622
 
 global __version__  # skipcq: PYL-W0604
 
-__version__ = """2.0.2"""
+__version__ = """2.0.3-rc-1"""
 """The version of this program.
 
 	Minimal Acceptance Testing:
