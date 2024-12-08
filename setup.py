1c6294f05747e89c0ff3afa3dd742169e6b7807e
Mr. Walls <reactive-firewall@users.noreply.github.com>
2024-12-08 00:04:35 -0800

    [PATCH] minor tweak for performance (- WIP PR #248 -)
    Changes in file setup.py:
     - use set instead of array for whitelist.

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ def readFile(filename):
 	"""
 	theResult = None
 	try:
-		expected_files = ["""E.md""", """requirements.txt"""]
+		expected_files = {"""E.md""", """requirements.txt"""}
 		if not any(aexpected_file in filename for aexpected_file in expected_files):
 			raise ValueError(f"[CWE-706] Access to the file {filename} was not expected.") from None
 		with open(f"./{filename}") as f:
