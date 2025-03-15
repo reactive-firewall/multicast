d74f6bf4dd6addc59e5bbc221785a8cd49f57c5b
Mr. Walls <reactive-firewall@users.noreply.github.com>
2025-03-14 18:33:57 -0700

    [UPDATE] Version v2.0.5

--- multicast/__init__.py
+++ multicast/__init__.py
@@ -114,7 +114,7 @@ __name__ = "multicast"  # skipcq: PYL-W0622
 
 global __version__  # skipcq: PYL-W0604
 
-__version__ = "2.0.4"
+__version__ = "2.0.5"
 """The version of this program.
 
 	Minimal Acceptance Testing:
