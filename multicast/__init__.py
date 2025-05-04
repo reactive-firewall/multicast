62746b045fcd1d548423d207e8b067ee1d3c6ea5
Mr. Walls <reactive-firewall@users.noreply.github.com>
2025-05-03 21:39:38 -0700

    [UPDATE] Sync with v2.0.8
    Changes in file docs/conf.py:
     * related work
    
    Changes in file multicast/__init__.py:
     * related work
    
    Changes in file setup.cfg:
     * related work

--- multicast/__init__.py
+++ multicast/__init__.py
@@ -117,7 +117,7 @@ __name__ = "multicast"  # skipcq: PYL-W0622
 
 global __version__  # skipcq: PYL-W0604
 
-__version__ = "2.0.7"
+__version__ = "2.0.8"
 """The version of this program.
 
 	Minimal Acceptance Testing:
