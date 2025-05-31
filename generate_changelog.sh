94e82bd7d81c305cdf06e0f96ca372377aa8349e
Mr. Walls <reactive-firewall@users.noreply.github.com>
2025-05-30 21:44:48 -0700

    [FEATURE] Improved initial implementation a bunch (- WIP #155 -)
    Changes in file generate_changelog.sh:
     * added some caching while re-generationg
     * now generates new changelog from scratch if given no options
     * now extracts impacted issues and kinds of changes as well as per-file changes
     * improved initial formatting a bit

--- generate_changelog.sh
+++ generate_changelog.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
 
+# Declare a varaiable for caching
+declare cache;
+
 function id_current_tag() {
 	local INPUT="${1}"
 	git describe --tags --abbrev=0 ${INPUT}
 }
 
-function enumerate_tag_history() {
+function run_enumerate_tag_history() {
 	local HEAD_TAG=$(id_current_tag HEAD)
 	{ git rev-list --tags --ancestry-path "$(git tag --no-contains "${HEAD_TAG}" | sort -V | head -n1)^..HEAD" ; wait ;} | xargs -I{} git tag --points-at "{}" 2>/dev/null ;
+	wait ;
+}
+
+function enumerate_tag_history() {
+	if [[ -z "${cache}" ]] ; then
+		cache=$(run_enumerate_tag_history | grep -vE "v?\d+.\d+.\d+-dev" ; wait;)
+	fi ;
+	printf "%s\n" "${cache}" ;
+	wait ;
+}
+
+function id_parent_tag() {
+	local INPUT="${1}"
+	enumerate_tag_history | grep -A 1 -F -f <(id_current_tag ${INPUT} ) | tail -n1
 	wait ;
 }
 
@@ -15,9 +32,14 @@ function enumerate_tag_history() {
 # then construct a Git range based on these tags. If no argument is provided, it defaults to
 # using the tags.
 if [[ -z "${1}" ]] ; then
-GIT_CURRENT_TAG=$(id_current_tag)
-GIT_PREVIOUS_TAG=$(enumerate_tag_history | grep -A 1 -F -f <(git describe --tags --abbrev=0 "${GIT_CURRENT_TAG}") | tail -n1 )
-FALLBACK_GIT_RANGE="${GIT_CURRENT_TAG}...${GIT_PREVIOUS_TAG}"
+# GIT_CURRENT_TAG=$(id_current_tag)
+
+for EACH_TAG in $(enumerate_tag_history | sort -Vr) ; do
+GIT_PREVIOUS_TAG=$(id_parent_tag "${EACH_TAG}")
+FALLBACK_GIT_RANGE="${EACH_TAG}...${GIT_PREVIOUS_TAG}"
+"${0}" "${FALLBACK_GIT_RANGE}" || : ; wait ;
+done;
+exit 0 ;
 fi ;
 GIT_RANGE="${1:-${FALLBACK_GIT_RANGE}}"
 
@@ -25,7 +47,19 @@ GIT_RANGE="${1:-${FALLBACK_GIT_RANGE}}"
 CHANGELOG_BUFFER=".chagelog_buffer.txt"
 cat <(git log "${GIT_RANGE}" --reverse --pretty=format:"COMMIT_START%n%h%n%B%nCOMMIT_END") >"${CHANGELOG_BUFFER}" ; wait ;
 
+RAW_IMPACTED_ISSUES=$(cat <"${CHANGELOG_BUFFER}" | grep -oE "([#]\d+){1}\b" | sort -id | uniq | xargs -L1 -I{} printf "%s, " "{}" ; wait ;)
+RAW_FLAGS_USED=$(cat <"${CHANGELOG_BUFFER}" | grep -oE "^([\[][A-Z]+[]]){1}" | sort -id | uniq -c | sort -rid | grep -oE "([A-Z]+){1}" | xargs -L1 -I{} printf "%s, " "{}" ; wait ;)
+
+
+# header
 printf "%s\n\n" "## What Changed between \`${GIT_RANGE}\`" ;
+# insights table
+printf "%s\n%s\n" "| Details | |" "|------------------|---------------------|" ;
+printf "%s" "| Kinds of changes | " ;
+printf "%s\n" "${RAW_FLAGS_USED:-'None'} |" | sed -e 's/,  |/ |/g' ;
+printf "%s" "| Impacted Issues | " ;
+printf "%s\n" "${RAW_IMPACTED_ISSUES:-'None'} |" | sed -e 's/,  |/ |/g' ;
+printf "\n\n" ;
 
 # auto-collect gitlog
 for FILE_INPUT in $(git log --pretty=format:"%n" --name-only "${GIT_RANGE}" | sort -id | uniq) ; do
@@ -60,6 +94,8 @@ awk -v RS='' -v ORS='\n\n\n' -v filename="$FILE_INPUT" '
 fi ;
 
 done ;
+
+unset cache 2>/dev/null || : ;
 
 # remove the buffer
 rm -f "${CHANGELOG_BUFFER}" 2>/dev/null || : ;
